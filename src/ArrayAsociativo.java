ecf7503b43fb100d34eb1955897233e77b90a810
Esteban <estebanda_92@hotmail.com>
2016-05-20 17:27:28 +0200

    devolverBooleanoParaSaberSiLaClaveExisteSinDatosEnArray.codigo

--- src/ArrayAsociativo.java
+++ src/ArrayAsociativo.java
@@ -8,12 +8,15 @@ public class ArrayAsociativo {
 	}
 
 	public void put(String clave, String valor) {
-		if (primerNodo == null)	primerNodo = new Nodo(clave, valor);
-		else{
+		if (primerNodo == null)
+			primerNodo = new Nodo(clave, valor);
+		else {
 			Nodo current = primerNodo;
-			while (current.sig != null && current.clave != clave) current = current.sig;
-			if (clave == current.clave) current.valor = valor;
-			else{
+			while (current.sig != null && current.clave != clave)
+				current = current.sig;
+			if (clave == current.clave)
+				current.valor = valor;
+			else {
 				current.sig = new Nodo(clave, valor);
 			}
 		}
@@ -21,15 +24,27 @@ public class ArrayAsociativo {
 
 	public String get(String cl) {
 		Nodo current = primerNodo;
-		while (current.sig != null && current.clave != cl) current = current.sig;
-		if (current.clave != cl) throw new RuntimeException();
+		while (current.sig != null && current.clave != cl)
+			current = current.sig;
+		if (current.clave != cl)
+			throw new RuntimeException();
 		return current.valor;
 	}
 
 	public String getOrElse(String cl, String valorPorDefecto) {
 		Nodo current = primerNodo;
-		while (current.sig != null && current.clave != cl) current = current.sig;
+		while (current.sig != null && current.clave != cl)
+			current = current.sig;
+		if (current.clave != cl)
+			return valorPorDefecto;
 		return current.valor;
 	}
 
+	public boolean containsKey(String cl) {
+		if (primerNodo == null) return false;
+		Nodo current = primerNodo;
+		while (current.sig != null && current.clave != cl)
+			current = current.sig;
+		return (current.clave == cl);
+	}
 }
